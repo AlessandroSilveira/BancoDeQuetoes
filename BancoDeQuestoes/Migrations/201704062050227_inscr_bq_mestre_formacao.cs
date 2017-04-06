3b342dac6a70808d0ca20bfec64e7bf91ee732a3
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-04-06 18:04:08 -0300

    try update database

--- BancoDeQuestoes/Migrations/201704062050227_inscr_bq_mestre_formacao.cs
+++ BancoDeQuestoes/Migrations/201704062050227_inscr_bq_mestre_formacao.cs
@@ -7,7 +7,15 @@ namespace BancoDeQuestoes.Migrations
     {
         public override void Up()
         {
-        }
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "ID_MESTRE_FORMACAO", c => c.Int());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "ID_MESTRE", c => c.Int());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_TIPO_FORMACAO", c => c.String());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_CURSO", c => c.String());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_INSTITUICAO", c => c.String());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_COPIA_DIPLOMA", c => c.Boolean());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_DIPLOMA_AUTENTICADO", c => c.Boolean());
+			AddColumn("dbo.INSCR_BQ_MESTRE_FORMACAO", "DESC_NUMERO_COPIAS", c => c.Int());
+		}
         
         public override void Down()
         {
